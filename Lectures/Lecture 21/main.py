f50bfd9c05f75dac7fbcf92703f29dd60b47b347
Elorie Bernard-Lacroix <eloriel0310@gmail.com>
2023-03-10 12:01:50 -0500

    lecture 21

---
+++ Lectures/Lecture 21/main.py
@@ -0,0 +1,10 @@
+import linkedlist
+
+# from linkedlist import LinkedList
+
+if __name__ == '__main__':
+    LL = LinkedList()
+    LL.append(4)
+    LL.append(10)
+    LL.append(12)
+    print(LL)
---
+++ tests.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+  char x = 500;
+  if(x==500) printf("This is true.\n");
+  else  printf("I don't think so!\n");
+
+}
